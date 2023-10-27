0670355658e9d2d0d953c939b04750257d974280
ProfessorAguiar <aguiar.professor@outlook.com>
2023-10-27 16:57:27 -0300

    Commit aula 27/10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  public logado=false
+  public loginTest:string='root'
+  public senhaTest:string='123456'
+  public logar(login:any, senha:any) {
+    if(login===this.loginTest && senha===this.senhaTest){
+      this.logado=true
+    }
+  }
   constructor() {}
 }
